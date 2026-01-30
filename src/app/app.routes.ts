a2c539a7909ddb7df00cef144c25d6e0564a7dc0
Pedro Santos <oliveirasantospedro732@gmail.com>
2026-01-30 10:57:19 -0300

    feat: create not-ound component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { Home } from './components/home/home';
 import { Counter } from './components/counter/counter';
+import { NotFound } from './components/not-found/not-found';
 
 export const routes: Routes = [
     {
@@ -10,5 +11,9 @@ export const routes: Routes = [
     {
         path: "counter",
         component: Counter
+    },
+    {
+        path: "**",
+        component: NotFound
     }
 ];
